ae7848b1b6148698bc88d0b1236afac804622ee3
TerrorK1ng <Claudir.santosgoes010@gmail.com>
2023-12-19 13:10:35 -0300

    teste

---
+++ cadastro.py
@@ -0,0 +1,51 @@
+try: # System developed by Claudir Santos. Github: http://github.com/#claudir_Dev
+    selector = int(input("""
+                    \033[31m Cadastro de alunos - by Claudir\033[m
+            \033[32m*Cadastre um aluno e tire a média de sua nota final*\033[m \n
+    1- Cadastrar alunos
+    2- Encerrar programa\n
+    """))
+        
+    def calculateAverageNotes():
+        try:
+            listValueNotes = []
+            getAmontNotes = int(input(f"\nDigite a quantidade de notas para fazer a média de {getStudentName}: \n"))
+            for i in range(getAmontNotes):
+                valueNotes = int(input(f"\nDigite a {i+1}° Nota de {getStudentName}:"))
+                listValueNotes.append(valueNotes)
+            if listValueNotes:
+                calculateAverage = sum(listValueNotes) / len(listValueNotes)
+                formatAverageCalculated = "{:.2f}".format(calculateAverage)
+                print(f"""
+                \033[34mMédia Final\033[m    
+                Aluno: {getStudentName}\n
+                Idade: {getStudentAge}\n
+                Média: {formatAverageCalculated}\n
+                Situação: -""")
+        except ValueError:
+            print("\n\033[31m|ERRO| Digite um número\033[m")
+
+    if selector not in [1, 2]:
+        raise ValueError("Selecione uma das opções!")
+    if selector == 2:
+        exit()
+        
+    getStudentName = input("\nDigite o nome do Aluno: \n")
+    if getStudentName.replace(' ','').isalpha():
+        getStudentAge = input(f"\nDigite a idade de {getStudentName}: \n")
+        if getStudentAge.isdigit():
+            convertStudentAgeToInt = int(getStudentAge)
+            calculateAverageNotes()
+        else:
+            raise ValueError("Digite um número")
+    else:
+        raise ValueError("Digite o nome do Aluno! \n")
+
+except ValueError as e:
+    print("\033[31m|ERRO| ", e)
+
+
+finally: print("\n\033[32mPrograma encerrado!\033[m")
+# TODO: Requisitar média de aprovação e falar se o aluno foi aprovado ou não.
+
+# TODO: Método Etec ou escola convencional
